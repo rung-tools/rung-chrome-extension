87ada29671c966640daff88826b349af68d35921
Marcelo Camargo <marcelocamargo@linuxmail.org>
2018-01-04 09:08:04 -0200

    Port webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const public = path.join(__dirname, 'public')
+const resources = path.join(public, 'resources')
+
+const deathPlugin = function () {
+    this.plugin('done', stats => {
+        const { errors = [] } = stats.compilation
+        if (errors.length > 0) {
+            errors.forEach(({ message }) => console.error(message))
+            process.exit(errors.length)
+        }
+    })
+}
 
 module.exports = {
-  entry: './src/index.bs.js',
-  output: {
-    path: path.join(__dirname, "build"),
-    filename: 'index.js',
-  },
+    entry: './src/index.bs.js',
+    output: {
+        path: public,
+        filename: '[name].min.js',
+    }
 };
